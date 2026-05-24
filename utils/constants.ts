a816b6213667935478c4f0aa2035ba5e311136ad
nelsonelaye <nelsonelaye@gmail.com>
2026-05-24 21:10:11 +0100

    refactor: update event link

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,7 +4,7 @@ export const volunteerLink =
   "https://docs.google.com/forms/d/e/1FAIpQLSdmRsqqLJNUQYR43sNjEfKqspMZB9d3yS945V26JR6mrkrKUA/viewform";
 
 export const eventLink =
-  "https://www.usetika.africa/events/ai-at-work-19dd18eb";
+  "https://www.usetika.africa/events/build-your-first-ai-agent-fe14c06c";
 
 export const substackLink = "https://substack.com/@ailagos";
 
